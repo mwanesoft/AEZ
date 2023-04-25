6def9849dc784431f73de1d9ef8478d3e420b4d7
Ailton Baúque <nevskya@hotmail.com>
2023-04-25 14:07:22 +0200

    UPDATE: extend the AppUSer and create a bank acc model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+namespace AEZ.Models;
+
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsAdmin { get; set; }
+}
