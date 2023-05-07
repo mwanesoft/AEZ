727189c2051829748a5f657947747d728036bca9
Ailton Baúque <nevskya@hotmail.com>
2023-05-07 16:56:08 +0200

    deleting ffiles

--- Models/BankAccount.cs
+++ Models/BankAccount.cs
@@ -1,13 +1,13 @@
-namespace AEZ.Models;
-
-public class BankAccount
-{
-    public int Id { get; set; }
-    public string AccountNumber { get; set; }
-    public string RoutingNumber { get; set; }
-    public decimal Balance { get; set; }
-    public string OwnerId { get; set; }
-
-    // Navigation property for the account owner
-    public virtual ApplicationUser Owner { get; set; }
-}
+// namespace AEZ.Models;
+//
+// public class BankAccount
+// {
+//     public int Id { get; set; }
+//     public string AccountNumber { get; set; }
+//     public string RoutingNumber { get; set; }
+//     public decimal Balance { get; set; }
+//     public string OwnerId { get; set; }
+//
+//     // Navigation property for the account owner
+//     public virtual ApplicationUser Owner { get; set; }
+// }
