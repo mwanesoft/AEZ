e11e389259ee699285a2fec9d75f3f2aa07ba81c
Ailton Baúque <nevskya@hotmail.com>
2023-05-07 16:53:36 +0200

    reseting the Context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using AEZ.Models;
 
 public class ApplicationDbContext : IdentityDbContext
 {
@@ -14,8 +13,5 @@ public class ApplicationDbContext : IdentityDbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        builder.Entity<ApplicationUser>().Property(u => u.FirstName).HasMaxLength(50);
-        builder.Entity<ApplicationUser>().Property(u => u.LastName).HasMaxLength(50);
-        builder.Entity<ApplicationUser>().Property(u => u.IsAdmin).HasDefaultValue(false);
     }
 }
